5c4d3feec486f51608fe022c0439e9d471f9bf78
Lance Valle <vallelance8@gmail.com>
2022-11-25 17:14:30 +0800

    exponents notes

---
+++ script.js
@@ -0,0 +1,16 @@
+// Math Algebra
+let base = 3
+let exponent =  2
+
+console.log(base**2)
+
+// the product rules
+console.log(base**exponent * base**exponent)
+console.log(base**(exponent+exponent))
+// - when multiply two expressions with the same base
+// - add the exponent
+
+
+// the quotient rule.
+console.log(base**6 / base**2)
+console.log(base**(6-2))
