87b3635c3e823a81c6e8f90560dae6a65b2358f1
Lance Valle <vallelance8@gmail.com>
2022-11-25 20:23:02 +0800

    exponents add notes

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ console.log(base**(exponent+exponent))
 // the quotient rule.
 console.log(base**6 / base**2)
 console.log(base**(6-2))
+
+//the power rule
+console.log((5**4)**3)
+console.log(5**12)
+
+//the power of zero
+console.log(5**0)
+console.log(5**3/5**3)
+
+//Negative exponents
+console.log(5**-1)
+console.log(1/5**1)
+
+//Fractional exponents
+console.log(64**(1/3))
+console.log(Math.cbrt(64))
+
+//Distribute an Exponent over a product
+console.log((5*7)**3)
+console.log((5**3) * (7**3))
+ 
+// Distribute an Exponent over a quotient
+console.log((5/7)**5)
+console.log((5**5)/(7**5))
